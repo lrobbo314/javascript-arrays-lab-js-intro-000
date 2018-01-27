022c598ed541cac791e6857dd56f714bde934a62
Robert Lego <robertlego@gmail.com>
2018-01-27 07:29:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ var kittens = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendKitten(name) {
   kittens.push(name);
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.shift(name);
+}
